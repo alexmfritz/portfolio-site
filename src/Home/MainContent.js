00199ac9e954e7924c242b2df40595e3e2161626
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-28 14:48:27 -0800

    Finish conversion to SCSS nesting

--- src/Home/MainContent.js
+++ src/Home/MainContent.js
@@ -4,13 +4,11 @@ import climbTwo from './images/IMG_4690.jpg';
 
 export default function MainContent() {
   return (
-    <main className="main">
-        <div className="flex column center">
-          <img className="image scale-in-center" src={climbTwo} alt="climbing"></img>
-          <p className="p-intro focus-in-expand-fwd">Hello! Welcome to my first creation with React!
+    <main className="flex column center">
+          <img src={climbTwo} alt="climbing"></img>
+          <p>Hello! Welcome to my first creation with React!
             I am currently a Front-End Software Developer student at Turing School of Software & Design.
           </p>
-        </div>
     </main>
   )
 }
