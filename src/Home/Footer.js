acdf1e5f51ba3163fa032a9a5144a24a4d6d7495
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-28 15:02:02 -0800

    Clean up small syntax errors

--- src/Home/Footer.js
+++ src/Home/Footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
       <section className="footer-nav-box">
         <ul className="footer-nav-items flex">
           <li className="puff-in-center-one"><a href="https://www.linkedin.com/in/alexmfritz/"><i class="fab fa-linkedin fa-2x"></i></a></li>
-          <li className="puff-in-center-two"><a href="https://github.com/alexmfritz"></a><i class="fab fa-github-square fa-2x"></i></li>
-          <li className="puff-in-center-three"><a href="mailto:alexfritzdev@gmail.com"><i class="fas fa-envelope-square fa-2x"></i></a></li>
+          <li className="puff-in-center-two"><a href="https://github.com/alexmfritz"><i class="fab fa-github-square fa-2x"></i></a></li>
+          <li className="puff-in-center-three"><a href="mailto:alexfritzdev@gmail.com"><i class="fas fab fa-envelope-square fa-2x"></i></a></li>
         </ul>
       </section>
       <section className="footer-design-box end">
--- src/Home/MainContent.js
+++ src/Home/MainContent.js
@@ -6,7 +6,7 @@ export default function MainContent() {
   return (
     <main className="flex column center">
           <img src={climbTwo} alt="climbing"></img>
-          <p>Hello! Welcome to my first creation with React!
+          <p>Hello! Welcome to my first creation with React and Sass!
             I am currently a Front-End Software Developer student at Turing School of Software & Design.
           </p>
     </main>
